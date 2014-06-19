21fda663144263ca737c9741295b955cbf1110c0
cwc <cwc1987@163.com>
2014-06-19 22:45:13 +0800

    Add Lesson03, Lesson04.

--- Lesson02_CreatePolygon/shader/fragshader.glsl
+++ Lesson02_CreatePolygon/shader/fragshader.glsl
@@ -1,11 +1,9 @@
 #ifdef GL_ES
-// Set default precision to medium
 precision mediump int;
 precision mediump float;
 #endif
 
 void main()
 {
-    // Set fragment color from texture
     gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
--- Lesson02_CreatePolygon/shader/vertshader.glsl
+++ Lesson02_CreatePolygon/shader/vertshader.glsl
@@ -1,5 +1,4 @@
 #ifdef GL_ES
-// Set default precision to medium
 precision mediump int;
 precision mediump float;
 #endif
